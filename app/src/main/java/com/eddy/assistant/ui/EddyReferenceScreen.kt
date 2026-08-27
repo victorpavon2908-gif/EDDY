89cb6655d09bbbdf5f08d1ff81ce30fa3934f1b6
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 11:11:05 -0600

    fix: remove invalid Compose weight import

--- app/src/main/java/com/eddy/assistant/ui/EddyReferenceScreen.kt
+++ app/src/main/java/com/eddy/assistant/ui/EddyReferenceScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
