3a01a408c0e2d08b489844d5d9609e407ea9d9e2
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 13:42:34 -0600

    chore: versionar EDDY 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.eddy.assistant"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
