38b2179a6ce1789c9f1b819efb1fbac79191513c
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 11:26:09 -0600

    feat: ampliar comandos y aplicaciones de EDDY

--- app/src/main/java/com/eddy/assistant/brain/AssistantCommand.kt
+++ app/src/main/java/com/eddy/assistant/brain/AssistantCommand.kt
@@ -6,6 +6,11 @@ sealed interface AssistantCommand {
     data object TellTime : AssistantCommand
     data object Greeting : AssistantCommand
     data object MemorySummary : AssistantCommand
+    data object ClearMemory : AssistantCommand
+    data class Dial(val number: String) : AssistantCommand
+    data class ComposeMessage(val number: String, val message: String) : AssistantCommand
+    data class SetAlarm(val hour: Int, val minute: Int, val label: String?) : AssistantCommand
+    data class OpenMaps(val query: String) : AssistantCommand
     data class Unknown(val originalText: String) : AssistantCommand
 }
 
@@ -13,4 +18,7 @@ enum class SupportedApp(val displayName: String, val packageName: String) {
     YOUTUBE("YouTube", "com.google.android.youtube"),
     WHATSAPP("WhatsApp", "com.whatsapp"),
     SPOTIFY("Spotify", "com.spotify.music"),
+    MAPS("Google Maps", "com.google.android.apps.maps"),
+    CHROME("Chrome", "com.android.chrome"),
+    GMAIL("Gmail", "com.google.android.gm"),
 }
