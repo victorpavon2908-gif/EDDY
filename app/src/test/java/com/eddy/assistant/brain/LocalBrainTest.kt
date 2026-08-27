d034a55a52282ab2ca37cfe6fb3a11826a228b0b
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 14:58:31 -0600

    test: cubrir funciones pro de EDDY

--- app/src/test/java/com/eddy/assistant/brain/LocalBrainTest.kt
+++ app/src/test/java/com/eddy/assistant/brain/LocalBrainTest.kt
@@ -11,7 +11,7 @@ class LocalBrainTest {
     fun opensYoutube() {
         assertEquals(
             AssistantCommand.OpenApp(SupportedApp.YOUTUBE),
-            brain.understand("EDDY, abre YouTube")
+            brain.understand("EDDY, abre YouTube"),
         )
     }
 
@@ -19,7 +19,7 @@ class LocalBrainTest {
     fun understandsCameraWithoutAccent() {
         assertEquals(
             AssistantCommand.OpenCamera,
-            brain.understand("abre la cámara")
+            brain.understand("abre la cámara"),
         )
     }
 
@@ -34,7 +34,7 @@ class LocalBrainTest {
     fun createsEveningAlarm() {
         assertEquals(
             AssistantCommand.SetAlarm(19, 30, "Alarma creada por EDDY"),
-            brain.understand("pon una alarma a las 7:30 pm")
+            brain.understand("pon una alarma a las 7:30 pm"),
         )
     }
 
@@ -42,15 +42,87 @@ class LocalBrainTest {
     fun createsTimerInMinutes() {
         assertEquals(
             AssistantCommand.SetTimer(180, "Temporizador creado por EDDY"),
-            brain.understand("pon un temporizador de 3 minutos")
+            brain.understand("pon un temporizador de 3 minutos"),
         )
     }
 
     @Test
-    fun composesMessageWithoutSendingAutomatically() {
+    fun composesSms() {
         assertEquals(
             AssistantCommand.ComposeMessage("+50588887777", "voy en camino"),
-            brain.understand("envía un mensaje al +505 8888 7777 diciendo voy en camino")
+            brain.understand("envía un mensaje al +505 8888 7777 diciendo voy en camino"),
+        )
+    }
+
+    @Test
+    fun preparesWhatsappMessage() {
+        assertEquals(
+            AssistantCommand.WhatsAppMessage("88887777", "ya voy"),
+            brain.understand("manda por WhatsApp al 8888 7777 diciendo ya voy"),
+        )
+    }
+
+    @Test
+    fun playsSpotifySearch() {
+        assertEquals(
+            AssistantCommand.PlaySpotify("música de Carlos Mejía Godoy"),
+            brain.understand("pon música de Carlos Mejía Godoy en Spotify"),
+        )
+    }
+
+    @Test
+    fun turnsTorchOn() {
+        assertEquals(
+            AssistantCommand.SetTorch(true),
+            brain.understand("prendé la linterna"),
+        )
+    }
+
+    @Test
+    fun setsMediaVolume() {
+        assertEquals(
+            AssistantCommand.SetVolume(45),
+            brain.understand("poné el volumen al 45%"),
+        )
+    }
+
+    @Test
+    fun opensWifiPanel() {
+        assertEquals(
+            AssistantCommand.OpenSystemPanel(SystemPanel.WIFI),
+            brain.understand("abrí el wifi"),
+        )
+    }
+
+    @Test
+    fun setsBrightness() {
+        assertEquals(
+            AssistantCommand.SetBrightness(40),
+            brain.understand("brillo al 40%"),
+        )
+    }
+
+    @Test
+    fun controlsSmartHomeLight() {
+        assertEquals(
+            AssistantCommand.SmartHomeControl("luz sala", false),
+            brain.understand("apagá la luz de la sala"),
+        )
+    }
+
+    @Test
+    fun opensSmartHomeConfiguration() {
+        assertEquals(
+            AssistantCommand.OpenSmartHomeSettings,
+            brain.understand("configura casa inteligente"),
+        )
+    }
+
+    @Test
+    fun reportsBattery() {
+        assertEquals(
+            AssistantCommand.BatteryStatus,
+            brain.understand("cuánta batería me queda"),
         )
     }
 
@@ -58,7 +130,7 @@ class LocalBrainTest {
     fun opensMapDestination() {
         assertEquals(
             AssistantCommand.OpenMaps("Masaya"),
-            brain.understand("llévame a Masaya")
+            brain.understand("llévame a Masaya"),
         )
     }
 
@@ -66,7 +138,7 @@ class LocalBrainTest {
     fun clearsMemoryOnExplicitRequest() {
         assertEquals(
             AssistantCommand.ClearMemory,
-            brain.understand("EDDY olvida todo")
+            brain.understand("EDDY olvida todo"),
         )
     }
 }
