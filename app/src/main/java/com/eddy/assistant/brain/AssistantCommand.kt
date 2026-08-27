259e0128c81e1509684fb3528c3cbfba56dfcb43
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 13:36:38 -0600

    feat: agregar comando de temporizador

--- app/src/main/java/com/eddy/assistant/brain/AssistantCommand.kt
+++ app/src/main/java/com/eddy/assistant/brain/AssistantCommand.kt
@@ -10,6 +10,7 @@ sealed interface AssistantCommand {
     data class Dial(val number: String) : AssistantCommand
     data class ComposeMessage(val number: String, val message: String) : AssistantCommand
     data class SetAlarm(val hour: Int, val minute: Int, val label: String?) : AssistantCommand
+    data class SetTimer(val seconds: Int, val label: String?) : AssistantCommand
     data class OpenMaps(val query: String) : AssistantCommand
     data class Unknown(val originalText: String) : AssistantCommand
 }
