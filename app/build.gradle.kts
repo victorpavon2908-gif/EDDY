1336849882abb6fabcc196bfb7cb7241a45f4dfe
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 17:45:24 -0600

    build: publicar EDDY v0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.eddy.assistant"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.3"
+        versionCode = 8
+        versionName = "0.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
