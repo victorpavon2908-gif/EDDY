b7350aa2dd17eeb9557fc14c98abdacf98123806
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-08-27 13:39:16 -0600

    test: cubrir comandos principales de EDDY

--- app/src/test/java/com/eddy/assistant/brain/LocalBrainTest.kt
+++ app/src/test/java/com/eddy/assistant/brain/LocalBrainTest.kt
@@ -27,5 +27,46 @@ class LocalBrainTest {
     fun unknownCommandKeepsOriginalText() {
         val result = brain.understand("compra comida")
         assertTrue(result is AssistantCommand.Unknown)
+        assertEquals("compra comida", (result as AssistantCommand.Unknown).originalText)
+    }
+
+    @Test
+    fun createsEveningAlarm() {
+        assertEquals(
+            AssistantCommand.SetAlarm(19, 30, "Alarma creada por EDDY"),
+            brain.understand("pon una alarma a las 7:30 pm")
+        )
+    }
+
+    @Test
+    fun createsTimerInMinutes() {
+        assertEquals(
+            AssistantCommand.SetTimer(180, "Temporizador creado por EDDY"),
+            brain.understand("pon un temporizador de 3 minutos")
+        )
+    }
+
+    @Test
+    fun composesMessageWithoutSendingAutomatically() {
+        assertEquals(
+            AssistantCommand.ComposeMessage("+50588887777", "voy en camino"),
+            brain.understand("envía un mensaje al +505 8888 7777 diciendo voy en camino")
+        )
+    }
+
+    @Test
+    fun opensMapDestination() {
+        assertEquals(
+            AssistantCommand.OpenMaps("Masaya"),
+            brain.understand("llévame a Masaya")
+        )
+    }
+
+    @Test
+    fun clearsMemoryOnExplicitRequest() {
+        assertEquals(
+            AssistantCommand.ClearMemory,
+            brain.understand("EDDY olvida todo")
+        )
     }
 }
